d4a50d6cc4695790cae8892d2fbe58ac3a99e919
josehfortes <jose@seja.dev>
2022-04-10 16:30:44 -0300

    Criado sistema de login

--- modules/user/user.schema.js
+++ modules/user/user.schema.js
@@ -9,3 +9,11 @@ export const signupSchema = Joi.object({
     .max(50).message('O campo "senha" pode ter no máximo {{#limit}} caracteres.')
     .min(6).message('O campo "senha" precisa ter no mínimo {{#limit}} caracteres.'),
 })
+
+
+export const loginSchema = Joi.object({
+  userOrEmail: Joi.string().required(),
+  password: Joi.string().required()
+    .max(50).message('O campo "senha" pode ter no máximo {{#limit}} caracteres.')
+    .min(6).message('O campo "senha" precisa ter no mínimo {{#limit}} caracteres.'),
+})
