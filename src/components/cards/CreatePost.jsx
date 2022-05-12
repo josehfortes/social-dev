565efa74a436b938ad4bfc4ebe2364331e216573
josehfortes <jose@seja.dev>
2022-05-12 12:04:37 -0300

    DESAFIO: criado estado de carregando no botão

--- src/components/cards/CreatePost.jsx
+++ src/components/cards/CreatePost.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 import { useForm } from 'react-hook-form'
 import { joiResolver } from '@hookform/resolvers/joi'
@@ -49,12 +50,20 @@ function CreatePost ({ username }) {
     resolver: joiResolver(createPostSchema),
     mode: 'all'
   })
+  const [loading, setLoading] = useState(false)
 
   const onSubmit = async (data) => {
-    const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/post`, data)
-    if (response.status === 201) {
-      reset()
-      mutate(`${process.env.NEXT_PUBLIC_API_URL}/api/post`)
+    try {
+      setLoading(true)
+      const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/post`, data)
+      if (response.status === 201) {
+        reset()
+        mutate(`${process.env.NEXT_PUBLIC_API_URL}/api/post`)
+      }
+    } catch (err) {
+      console.error(err)
+    } finally {
+      setLoading(false)
     }
   }
 
@@ -73,7 +82,7 @@ function CreatePost ({ username }) {
         </TextContainer>
         <BottomContainer>
           <BottomText>A sua mensagem será pública.</BottomText>
-          <Button disabled={!isValid}>Postar mensagem</Button>
+          <Button loading={loading} disabled={!isValid}>Postar mensagem</Button>
         </BottomContainer>
       </form>
     </PostContainer>
